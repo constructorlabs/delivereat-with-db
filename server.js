25191815dc3634fa6ed4d5d76e1dc0efb02ac664
Roland Levy <rolandjlevy@gmail.com>
2018-10-18 16:22:34 +0100

    Database and server.js up and running

--- server.js
+++ server.js
@@ -1,21 +1,81 @@
-const express = require('express');
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
+const pgp = require('pg-promise')();
+const express = require('express');
 const app = express();
+app.use('/static', express.static('static')); 
+app.set('view engine', 'hbs');
+
+const db = pgp({
+    host: 'localhost',
+    port: 5432,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD
+});
 
 app.use(bodyParser.json());
-app.use('/static', express.static('static'));
-app.set('view engine', 'hbs');
 
 const menu = {
   1: {
     id: 1,
-    name: "Strawberry cheesecake",
-    price: 6
+    item: "Mixed Salad",
+    price: "6.00",
+    img: "mixed-salad.jpg",
+    course: "starter"
+  },
+  2: {
+    id: 2,
+    item: "Fried Chicken",
+    price: "7.00",
+    img: "fried-chicken.jpg",
+    course: "starter"
   }
-};
+}
+
+const menu_purchases = {
+  "items": {
+      "1": { 
+          "id": 1, 
+          "quantity": 2 
+      },
+      "2": { 
+          "id": 2, 
+          "quantity": 3 
+      }
+  },
+  "name": "Matt",
+  "tel": "07901 972 811"
+}
 
 app.get('/', function(req, res){
   res.render('index');
+});
+
+app.get('/api/menu', function(req, res){
+    db.any('SELECT * FROM menu')
+      .then(data => res.json(data))
+      .catch(function(error) {
+          res.json({error: error.message});
+      });
+});
+
+app.post('/api/purchase', (req, res) =>{
+  // 1. insert items, name, tel and time from body request into "purchase" table
+  const { items, name, tel } = req.body; 
+  db.one(`INSERT INTO purchase (name, tel, created_at) VALUES($1, $2, NOW()) RETURNING id`, [name, tel]) 
+  .then(result => {
+    // set RETURNING id from INSERT INTO
+    const orderId = result.id; 
+    // 2. insert into "menu_purchase" table for each item
+    return Promise.all(Object.values(items).map(item => {
+      return db.none(`INSERT INTO menu_purchase (quantity, menu_id, purchase_id) VALUES($1, $2, $3)`, [item.quantity, item.id, orderId]);
+      }))
+      .then(() => orderId); // orderId is return when Promise.all is complete
+    })
+  .then(orderId => res.json({ orderId: orderId }))
+  .catch(error => res.json({ error: error.message }));
 });
 
 app.listen(8080, function(){
--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,40 @@ import '../styles/App.scss';
 class App extends React.Component {
   constructor(){
     super();
+
+    this.state = { 
+      menu: {}
+    }
+  }
+
+  /* get menu data
+  ///////////////////////////////////////////*/
+
+  componentDidMount () {
+    fetch("/api/menu")
+    .then(response => response.json())
+    .then(menu => {
+      this.setState({ menu })
+    })
   }
 
   render(){
     return (
       <div>
-        Delivereat app
+        { 
+          Object.values(this.state.menu).map(item => {
+            return (
+            <section key={item.id}>
+              <ul>
+                <li>{item.id}</li>
+                <li>{item.item}</li>
+                <li>{item.price}</li>
+                <li>{item.img}</li>
+                <li>{item.course}</li>
+              </ul>
+            </section>);
+          }) 
+        }
       </div>
     )
   }
