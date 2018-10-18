9e99b91a97b88a9600f19d2c48fdc040e6dc4d88
danGRT <d.geraghty06@gmail.com>
2018-10-18 16:46:04 +0100

    basic display of menu in place - issue with no div around menu categories to be addressed

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,49 @@
 import React from 'react';
+import Menu from './Menu.js'
 
 import '../styles/App.scss';
 
 class App extends React.Component {
   constructor(){
-    super();
+    super()
+
+    this.getMenu = this.getMenu.bind(this)
+
+    this.state = {
+      menu: {}
+    }
+
+
+}
+  componentDidMount(){
+  this.getMenu()
+}
+
+
+  arrayToObject(array){
+    return array.reduce((obj, item) => {
+      obj[item.id] = item
+      return obj
+    }, {})
+  }
+
+
+  getMenu(){
+    fetch('/api/menu/')
+      .then(response => response.json())
+      .then(body => {
+        const bodyObject = this.arrayToObject(body)
+        this.setState({
+          menu: bodyObject
+        })
+      })
   }
 
   render(){
     return (
       <div>
         Delivereat app
+        <Menu menu={this.state.menu}/>
       </div>
     )
   }
