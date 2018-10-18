d09fcc4a054ff6cd33d90d49268a70d0989fbb33
danGRT <d.geraghty06@gmail.com>
2018-10-18 14:45:38 +0100

    basic database functionality in place - can GET menu and POST orders

---
+++ database.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE delivereat;
+
+CREATE TABLE menu (
+  id serial,
+  name varchar(50) NOT NULL,
+  price numeric(4, 2) NOT NULL,
+  type varchar(50) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE orders (
+  id serial,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE order_map(
+  id serial,
+  menu_id INT,
+  orders_id INT,
+  quantity INT,
+  PRIMARY KEY(id),
+  FOREIGN KEY(menu_id) REFERENCES menu (id),
+  FOREIGN KEY(orders_id) REFERENCES orders (id)
+);
+
+
+INSERT INTO menu (name, price, type) VALUES ('Beefburger', 10.00, 'Burgers');
+INSERT INTO menu (name, price, type) VALUES ('Cheeseburger', 12.00, 'Burgers');
+INSERT INTO menu (name, price, type) VALUES ('Stilton burger', 14.50, 'Burgers');
+INSERT INTO menu (name, price, type) VALUES ('Lamb Burger', 13.00, 'Burgers');
+INSERT INTO menu (name, price, type) VALUES ('Chips', 4.00, 'Sides');
+INSERT INTO menu (name, price, type) VALUES ('Onion Rings', 4.00, 'Sides');
+INSERT INTO menu (name, price, type) VALUES ('Cajun Fries', 5.00, 'Sides');
+INSERT INTO menu (name, price, type) VALUES ('Coleslaw', 3.00, 'Sides');
+INSERT INTO menu (name, price, type) VALUES ('Strawberry Cheesecake', 7.00, 'Desserts');
+INSERT INTO menu (name, price, type) VALUES ('Churros', 6.00, 'Desserts');
+INSERT INTO menu (name, price, type) VALUES ('Ice Cream', 6.00, 'Desserts');
+INSERT INTO menu (name, price, type) VALUES ('Lemonade', 3.00, 'Drinks');
+INSERT INTO menu (name, price, type) VALUES ('Cola', 3.00, 'Drinks');
+INSERT INTO menu (name, price, type) VALUES ('Banana Milkshake', 4.50, 'Drinks');
+
+
+
+
+INSERT INTO orders VALUES (1);
+ALTER SEQUENCE orders_id_seq RESTART WITH 2 INCREMENT BY 1;
+
+INSERT INTO order_map (menu_id, quantity, orders_id) VALUES (1, 2, 1);
+INSERT INTO order_map (menu_id, quantity, orders_id) VALUES (5, 2, 1)
--- server.js
+++ server.js
@@ -1,22 +1,63 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const pgp = require('pg-promise')();
+const db =  pgp({
+  host: 'localhost',
+  port: 5432,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD
+})
 
 app.use(bodyParser.json());
 app.use('/static', express.static('static'));
 app.set('view engine', 'hbs');
 
-const menu = {
-  1: {
-    id: 1,
-    name: "Strawberry cheesecake",
-    price: 6
-  }
-};
 
 app.get('/', function(req, res){
   res.render('index');
 });
+
+
+
+//get menu
+app.get('/api/menu/', (req, res) => {
+  db.any('SELECT * FROM menu')
+  .then(data => {
+    res.json(data)
+  })
+  .catch(error => {
+    res.json({error: error.message})
+  })
+})
+
+
+//post an order
+app.post('/api/orders', (req, res) => {
+  db.one('INSERT INTO orders VALUES (default) RETURNING id;')
+  .then(response => {
+    const orderId = response.id
+    const order = req.body
+    const orderKeys = Object.keys(order)
+    return Promise.all(orderKeys.map(item => {
+      const menuId = order[item].menuId
+      const quantity = order[item].quantity
+      return db.none('INSERT INTO order_map (menu_id, quantity, orders_id)VALUES($1, $2, $3)', [menuId, quantity, orderId])
+    }))
+      .then(() => orderId)
+  })
+  .then(orderId => {
+    res.json({orderId})
+  })
+  .catch(error => {
+    res.json({error: error.message})
+  })
+})
+
+
 
 app.listen(8080, function(){
   console.log('Listening on port 8080');
